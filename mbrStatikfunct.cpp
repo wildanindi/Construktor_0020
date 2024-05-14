b45d5ccae0cc4a17162fd8fdc7db41dbe53e0554
wildanindi <wildanindi2@gmail.com>
2024-05-14 18:22:19 +0700

    project baru dengan class mahasiswa yang berisi 8 member yang terdiri variabel, method, dan constructor

---
+++ mbrStatikfunct.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+    private:
+        static int nim;
+
+    public:
+        int id;
+        string nama;
+
+        void setID();
+        void printAll();
+
+        static void setNim(int pNim) { nim = pNim; }
+        static int getNim() { return nim; }
+
+        mahasiswa (string pnama){
+            nama = pnama;
+            setID();
+        }    
+};
