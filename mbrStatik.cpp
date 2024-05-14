da02bfbd17aa2b5e286445e06a3366a056ccab70
wildanindi <wildanindi2@gmail.com>
2024-05-14 17:29:27 +0700

    menambah class dan member pada variabel, method, dan constructor

--- mbrStatik.cpp
+++ mbrStatik.cpp
@@ -1,2 +1,20 @@
 #include <iostream>
 using namespace std;
+
+class mahasiswa
+
+{
+    public:
+        static int nim;
+        int id;
+        string nama;
+
+        void setID ();
+
+        void printAll();
+        mahasiswa(string pnama)
+        {
+            nama = pnama;
+            setID();
+        }
+};
